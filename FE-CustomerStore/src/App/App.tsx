fbcbef7fc8b0ebb57a0e85c52be33458161681ad
Kyle Alexander <mcphylus@live.co.uk>
2022-01-03 15:52:23 +0000

    Added basic site layout

--- FE-CustomerStore/src/App/App.tsx
+++ FE-CustomerStore/src/App/App.tsx
@@ -6,12 +6,36 @@ import Home from "../Pages/Home";
 
 function App() {
   return (
-    <div>
-    <Navbar />
-      <Routes>
-        <Route path="/admin" element={ <Admin /> } />
-        <Route path="/" element={ <Home /> } />
-      </Routes>
+    <div className="container mx-auto">
+      <div className="w-full">
+        <Navbar />
+      </div>
+      <div className="w-full">
+        <img src="https://via.placeholder.com/150" />
+      </div>
+      <div className="w-full">
+        <Navbar />
+      </div>
+      <div className="w-full flex flex-row items-stretch">
+        <div className="flex-none w-1/5">
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </ul>
+        </div>
+        <div className="grow">
+          <Routes>
+            <Route path="/admin" element={ <Admin /> } />
+            <Route path="/" element={ <Home /> } />
+          </Routes>
+        </div>
+      </div>
+      <div className="w-full">
+        <Navbar />
+      </div>
     </div>
   );
 }
